5c06b4f80994063c3bf7ffc46d0bc510e3535627
shev1111 <evhen.shev@gmail.com>
2018-11-25 19:32:52 +0200

    create connection to postgres database

---
+++ src/com/shev/amazon_data/dao/ConnectionDB.java
@@ -0,0 +1,36 @@
+package com.shev.amazon_data.dao;
+
+
+
+import com.shev.amazon_data.utils.JdbcPropManager;
+import org.apache.log4j.Logger;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class ConnectionDB {
+    private static Logger logger = Logger.getLogger(ConnectionDB.class.getName());
+    private static String host = JdbcPropManager.getProperty("pg.host");
+    private static String port = JdbcPropManager.getProperty("pg.port");
+    private static String user = JdbcPropManager.getProperty("pg.user_name");
+    private static String password = JdbcPropManager.getProperty("pg.password");
+    private static String db = JdbcPropManager.getProperty("pg.db_name");
+    private static String url = "jdbc:postgresql://"+host+":"+port+"/"+db;
+    private static String driverName = "org.postgresql.Driver";
+    private static Connection connection;
+
+    public static Connection getConnection() {
+        try {
+            Class.forName(driverName);
+            try {
+                connection = DriverManager.getConnection(url, user, password);
+            } catch (SQLException e) {
+                logger.error("Failed to create the database connection "+e);
+            }
+        } catch (ClassNotFoundException e) {
+            logger.error("Driver not found. "+e);
+        }
+        logger.info("connection to "+db+" database was created");
+        return connection;
+    }
+}
--- src/com/shev/amazon_data/test.java
+++ src/com/shev/amazon_data/test.java
@@ -1,7 +1,9 @@
 package com.shev.amazon_data;
 
+import com.shev.amazon_data.dao.ConnectionDB;
 import com.shev.amazon_data.service.BuyingService;
 import com.shev.amazon_data.service.RegisterService;
+import com.shev.amazon_data.utils.JdbcPropManager;
 import org.apache.commons.validator.routines.EmailValidator;
 import org.openqa.selenium.WebDriver;
 
@@ -11,6 +13,7 @@ public class test {
        // WebDriver driver = BuyingService.addItemToCartByASIN("B07GC1HJ89", "relenocewe@daabox.com", "12345678");
        /* WebDriver driver = BuyingService.addItemToCartByLink("https://www.amazon.com/Storage-Childrens-Organizer-Comfortable-Seating/dp/B0788FKHDX/ref=sr_1_1_sspa?s=movies-tv&ie=UTF8&qid=1542527941&sr=1-1-spons&keywords=chair&th=1",
                 "relenocewe@daabox.com", "12345678");*/
+        ConnectionDB.getConnection();
 
 
 
