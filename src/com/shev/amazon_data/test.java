fa46247da3aa6e5b9f152853256a7e66037ef33a
shev1111 <evhen.shev@gmail.com>
2018-12-08 20:11:00 +0200

    csv lib

--- src/com/shev/amazon_data/test.java
+++ src/com/shev/amazon_data/test.java
@@ -1,5 +1,6 @@
 package com.shev.amazon_data;
 
+import com.shev.amazon_data.dao.CartDAO;
 import com.shev.amazon_data.dao.ConnectionDB;
 import com.shev.amazon_data.service.BuyingService;
 import com.shev.amazon_data.service.RegisterService;
@@ -7,13 +8,22 @@ import com.shev.amazon_data.utils.JdbcPropManager;
 import org.apache.commons.validator.routines.EmailValidator;
 import org.openqa.selenium.WebDriver;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 public class test {
     public static void main(String[] args) {
         //WebDriver driver = BuyingService.addItemToCartByLink("https://www.amazon.com/VivoBook-Nanoedge-i5-8250U-Processor-Fingerprint/dp/B0762S8PYM/ref=sr_1_6?s=pc&ie=UTF8&qid=1542483879&sr=1-6&keywords=laptop", "relenocewe@daabox.com", "12345678");
        // WebDriver driver = BuyingService.addItemToCartByASIN("B07GC1HJ89", "relenocewe@daabox.com", "12345678");
        /* WebDriver driver = BuyingService.addItemToCartByLink("https://www.amazon.com/Storage-Childrens-Organizer-Comfortable-Seating/dp/B0788FKHDX/ref=sr_1_1_sspa?s=movies-tv&ie=UTF8&qid=1542527941&sr=1-1-spons&keywords=chair&th=1",
                 "relenocewe@daabox.com", "12345678");*/
-        ConnectionDB.getConnection();
+       // ConnectionDB.getConnection();
+        String pattern = "yyyy-MM-dd HH:mm:ss";
+        String from = "2018-12-03 19:27:11";
+        String to = "2018-12-08 10:30:45";
+        SimpleDateFormat dateFormat = new SimpleDateFormat(pattern);
+
 
 
 
