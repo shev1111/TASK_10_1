388ba33285cf98995d3f7b6868932c940a6cfc93
shev1111 <evhen.shev@gmail.com>
2018-12-08 20:02:32 +0200

    test export to csv

--- test/com/shev/amazon_data/dao/CartDAOTest.java
+++ test/com/shev/amazon_data/dao/CartDAOTest.java
@@ -6,9 +6,17 @@ import com.shev.amazon_data.model.Item;
 import com.shev.amazon_data.model.SimpleItem;
 import com.shev.amazon_data.model.User;
 import org.junit.Test;
+
+import java.io.File;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 import static org.junit.Assert.*;
 
 public class CartDAOTest {
+
+
     private static String testAsin = "ZJB25DE87";
     private static String testLogin = "test@gmail.com";
     private static String testUserName = "Test";
@@ -17,6 +25,9 @@ public class CartDAOTest {
     private static String testTitle = "Test";
     private static String testAvailability = "In Stock";
     private static int testPrice = 12345;
+    private static final String USER_DIR = System.getProperty("user.dir");
+    private static final String SEP = System.getProperty("file.separator");
+    private static final String CSV_FILE_TEST = USER_DIR+SEP+"src"+SEP+"com"+SEP+"shev"+SEP+"amazon_data"+SEP+"data"+SEP+"cart_period.csv";
     @Test
     public void insertCartItem() throws Exception {
         CartItem testCartItem = new CartItem(testAsin,testLogin);
@@ -46,6 +57,20 @@ public class CartDAOTest {
         assertEquals(testLogin,testRetrieveCartItem.getUser_login());
         boolean delete = ItemsDAO.deleteItem(testItem.getAsin());
         assertTrue(delete);
+    }
+
+    @Test
+    public void cartByPeriodToCSV() throws ParseException {
+        String pattern = "yyyy-MM-dd HH:mm:ss";
+        String from = "2018-12-03 19:27:11";
+        String to = "2018-12-08 10:30:45";
+        SimpleDateFormat dateFormat = new SimpleDateFormat(pattern);
+        Date dateFrom = dateFormat.parse(from);
+        Date dateTo = dateFormat.parse(to);
+        File csv = new File(CSV_FILE_TEST);
+        assertTrue(CartDAO.cartByPeriodToCSV(dateFrom.getTime(),dateTo.getTime()));
+        assertTrue(csv.exists());
+        csv.delete();
     }
 
 }
